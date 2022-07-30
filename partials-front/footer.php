c5dfe760f6a0a301559bb3c3e439ad79c2182c58
surya962 <surya19022001@gmail.com>
2022-07-30 20:33:46 +0530

    Sample commit

---
+++ partials-front/footer.php
@@ -0,0 +1,39 @@
+
+  <!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/sf-ultralight-filled/50/000000/phone.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluency/48/000000/mail.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/color/48/000000/linkedin.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Surya Kant Singh</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+
+</body>
+</html>
---
+++ partials-front/menu.php
@@ -0,0 +1,47 @@
+<?php include('config/constants.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Go Organic</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style-front.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            
+            <div class="logo">
+                <a href="#" title="Logo">
+                    <img src="images/logo1.jpg" alt="Basket Logo" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>">Home</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>categories.php">Categories</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>foods.php">Products</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Navbar Section Ends Here -->
